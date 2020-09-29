a681659d904da2c991cfb0ae12bf5421573c332c
Robert <Robert>
2020-09-29 19:06:59 +0000

    *** empty log message ***

--- lambda/src/main/java/com/robertmayer/lambda/Lambda.java
+++ lambda/src/main/java/com/robertmayer/lambda/Lambda.java
@@ -47,16 +47,16 @@ public class Lambda {
 
     static int getchar() { try {return System.in.read();} catch (Exception e) { throw new RuntimeException("I/O error reading"); } }
     static void gettoken() {
-      int index = 0;
-      while(is_space(look)) { look = getchar(); }
-      if (is_parens(look)) {
-        token[index++] = look;  look = getchar();
-      } else {
-        while(index < SYMBOL_MAX - 1 && look != EOF && !is_space(look) && !is_parens(look)) {
-          token[index++] = look;  look = getchar();
+        int index = 0;
+        while(is_space(look)) { look = getchar(); }
+        if (is_parens(look)) {
+            token[index++] = look;  look = getchar();
+        } else {
+            while(index < SYMBOL_MAX - 1 && look != EOF && !is_space(look) && !is_parens(look)) {
+                token[index++] = look;  look = getchar();
+            }
         }
-      }
-      token[index] = '\0';
+        token[index] = '\0';
     }
 
     static boolean is_pair(List x) { return x.objType == ObjType.List; }
@@ -91,39 +91,39 @@ public class Lambda {
     }
 
     static List intern(String sym) {
-      List _pair = symbols;
-      for ( ; _pair != null ; _pair = cdr(_pair)) {
-          if (sym.equalsIgnoreCase(car(_pair).car.atom)) {
-              return car(_pair).car.atom;
-          }
-      }
-      symbols = cons(sym, symbols);
-      return car(symbols).car.atom;
+        List _pair = symbols;
+        for ( ; _pair != null ; _pair = cdr(_pair)) {
+            if (sym.equalsIgnoreCase(car(_pair).car.atom)) {
+                return car(_pair).car.atom;
+            }
+        }
+        symbols = cons(sym, symbols);
+        return car(symbols).car.atom;
     }
 
     static List getobj() {
-      if (token[0] == '(') return getlist();
-      return intern(token);
+        if (token[0] == '(') return getlist();
+        return intern(token);
     }
 
     static List getlist() {
-      gettoken();
-      if (token[0] == ')') return null;
-      List tmp = getobj();
-      return cons(tmp, getlist());
+        gettoken();
+        if (token[0] == ')') return null;
+        List tmp = getobj();
+        return cons(tmp, getlist());
     }
 
     static void print_obj(List ob, boolean head_of_list) {
-      if (!is_pair(ob) ) {
-        System.out.print(ob != null ? ob.car.atom : "null" );
-      } else {
-        if (head_of_list) System.out.print('(');
-        print_obj(ob.car.list, true);
-        if (cdr(ob) != null) {
-          System.out.print(' ');
-          print_obj(cdr(ob), false);
-        } else System.out.print(')');
-      }
+        if (!is_pair(ob) ) {
+            System.out.print(ob != null ? ob.car.atom : "null" );
+        } else {
+            if (head_of_list) System.out.print('(');
+            print_obj(ob.car.list, true);
+            if (cdr(ob) != null) {
+                System.out.print(' ');
+                print_obj(cdr(ob), false);
+            } else System.out.print(')');
+        }
     }
 
     static UnaryOperator<List> fcons =     (List a) -> {  return cons(car(a), car(cdr(a)));  };
@@ -137,62 +137,62 @@ public class Lambda {
     static UnaryOperator<List> fwriteobj = (List a) -> {  print_obj(car(a), true); System.out.println(""); return e_true();  };
 
     static List evlist(List list, List env) {
-      /* http://cslibrary.stanford.edu/105/LinkedListProblems.pdf */
-      List head = null, insertPos = null;
-      for ( ; list != null ; list = cdr(list) ) {
-        List currentArg = cons(eval(car(list), env), null);
-        if (head == null) {
-            head = currentArg;
-            insertPos = head;
+        /* http://cslibrary.stanford.edu/105/LinkedListProblems.pdf */
+        List head = null, insertPos = null;
+        for ( ; list != null ; list = cdr(list) ) {
+            List currentArg = cons(eval(car(list), env), null);
+            if (head == null) {
+                head = currentArg;
+                insertPos = head;
+            }
+            else {
+                insertPos.next = currentArg;
+                insertPos = currentArg;
+            }
         }
-        else {
-            insertPos.next = currentArg;
-            insertPos = currentArg;
-        }
-      }
-      return head;
+        return head;
     }
 
     static List apply_primitive(UnaryOperator<List> primfn, List args) {
-      return primfn.apply(args);
+        return primfn.apply(args);
     }
 
     static List eval(List exp, List env) {
         if (is_atom(exp) ) {
-          for ( ; env != null; env = cdr(env) )
-            if (exp == car(car(env)))  return car(cdr(car(env)));
-          return null;
+            for ( ; env != null; env = cdr(env) )
+                if (exp == car(car(env)))  return car(cdr(car(env)));
+            return null;
         } else if (is_atom( car (exp))) { /* special forms */
-          if (car(exp) == intern("quote")) {
-            return car(cdr(exp));
-          } else if (car(exp) == intern("if")) {
-            if (eval (car(cdr(exp)), env) != null)
-              return eval (car(cdr(cdr(exp))), env);
-            else
-              return eval (car(cdr(cdr(cdr(exp)))), env);
-          } else if (car(exp) == intern("lambda")) {
-            return exp; /* todo: create a closure and capture free vars */
-          } else if (car(exp) == intern("apply")) { /* apply function to list */
-            List args = evlist (cdr(cdr(exp)), env);
-            args = car(args); /* assumes one argument and that it is a list */
-            return apply_primitive( eval(car(cdr(exp)), env).car.prim, args);
-          } else { /* function call */
-            List primop = eval (car(exp), env);
-            if (is_pair(primop)) { /* user defined lambda, arg list eval happens in binding  below */
-              return eval( cons(primop, cdr(exp)), env );
-            } else if (primop != null) { /* built-in primitive */
-              return apply_primitive(primop.car.prim, evlist(cdr(exp), env));
+            if (car(exp) == intern("quote")) {
+                return car(cdr(exp));
+            } else if (car(exp) == intern("if")) {
+                if (eval (car(cdr(exp)), env) != null)
+                    return eval (car(cdr(cdr(exp))), env);
+                else
+                    return eval (car(cdr(cdr(cdr(exp)))), env);
+            } else if (car(exp) == intern("lambda")) {
+                return exp; /* todo: create a closure and capture free vars */
+            } else if (car(exp) == intern("apply")) { /* apply function to list */
+                List args = evlist (cdr(cdr(exp)), env);
+                args = car(args); /* assumes one argument and that it is a list */
+                return apply_primitive( eval(car(cdr(exp)), env).car.prim, args);
+            } else { /* function call */
+                List primop = eval (car(exp), env);
+                if (is_pair(primop)) { /* user defined lambda, arg list eval happens in binding  below */
+                    return eval( cons(primop, cdr(exp)), env );
+                } else if (primop != null) { /* built-in primitive */
+                    return apply_primitive(primop.car.prim, evlist(cdr(exp), env));
+                }
             }
-          }
         } else if (car(car(exp)) == intern("lambda")) { /* should be a lambda, bind names into env and eval body */
-          List extenv = env, names = car(cdr(car(exp))), vars = cdr(exp);
-          for (  ; names != null; names = cdr(names), vars = cdr(vars) )
-            extenv = cons (cons(car(names),  cons(eval (car(vars), env), null)), extenv);
-          return eval (car(cdr(cdr(car(exp)))), extenv);
+            List extenv = env, names = car(cdr(car(exp))), vars = cdr(exp);
+            for (  ; names != null; names = cdr(names), vars = cdr(vars) )
+                extenv = cons (cons(car(names),  cons(eval (car(vars), env), null)), extenv);
+            return eval (car(cdr(cdr(car(exp)))), extenv);
         }
         System.out.println("cannot evaluate expression");
         return null;
-      }
+    }
 
     static void main(String argv[]) {
         List env = cons (cons(intern("car"),     cons(fcar, null)),
