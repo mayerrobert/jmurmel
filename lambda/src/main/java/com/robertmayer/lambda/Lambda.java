8561c9191bf9d2ce7c07a68cc2cc8fd194fec1fa
Robert <Robert>
2020-09-29 21:52:15 +0000

    *** empty log message ***

--- lambda/src/main/java/com/robertmayer/lambda/Lambda.java
+++ lambda/src/main/java/com/robertmayer/lambda/Lambda.java
@@ -13,46 +13,24 @@ public class Lambda {
     private InputStream in;
     private PrintStream out;
 
-    private abstract static class List {
-        List car() { throw new RuntimeException("not a pair"); }
-        List cdr() { return null; }
-
-        String atom() { throw new RuntimeException("not an atom"); }
-        UnaryOperator<List> prim() { throw new RuntimeException("not a primary function"); }
+    private enum ObjType {
+        Atom,
+        Prim,
+        List,
+        ;
     }
 
-    private static class Atom extends List {
-        String str;
-        List cdr;
-        Atom(String str, List cdr) { this.str = str; this.cdr = cdr; }
+    private static class Pair {
+        ObjType objType;
+        Object car;
+        Pair cdr;
 
-        @Override
-        String atom() { return str; }
-        @Override
-        List cdr() { return cdr; }
+        Pair(String str, Pair cdr)               { this.car = str; this.cdr = cdr; objType = ObjType.Atom; }
+        Pair(UnaryOperator<Pair> prim, Pair cdr) { this.car = prim; objType = ObjType.Prim; }
+        Pair(Pair car, Pair cdr)                 { this.car = car; this.cdr = cdr; objType = ObjType.List; }
     }
 
-    private static class LList extends List {
-        List car;
-        List cdr;
-
-        LList(List car, List cdr) { this.car = car; this.cdr = cdr; }
-
-        @Override
-        List car() { return car; }
-        @Override
-        List cdr() { return cdr; }
-    }
-
-    private static class Prim extends List {
-        UnaryOperator<List> prim;
-
-        Prim(UnaryOperator<List> prim) { this.prim = prim; }
-        @Override
-        UnaryOperator<List> prim() { return prim; }
-    }
-
-    private List symbols = null;
+    private Pair symbols = null;
 
     private int look; /* look ahead character */
 
@@ -74,136 +52,148 @@ public class Lambda {
         token[index] = '\0';
     }
 
-    private boolean is_pair(List x) { return x instanceof LList; }
-    private boolean is_atom(List x) { return x instanceof Atom; }
-    private List car(List x) { return x.car(); }
-    private List cdr(List x) { return x.cdr(); }
-    private List e_true() { return cons( intern("quote"), cons( intern("t"), null)); }
-    private List e_false() { return null; }
+    private boolean is_pair(Object x) { return x instanceof Pair; }
+    private boolean is_atom(Object x) { return x instanceof String; }
+    private Object car(Pair x) { return x.car; }
+    private Pair cdr(Pair x) { return x.cdr; }
+    private Pair e_true() { return cons( intern("quote"), cons( intern("t"), null)); }
+    private Pair e_false() { return null; }
 
-    private List cons(String _car, List _cdr) {
-        return new Atom(_car, _cdr);
+    private Pair cons(String _car, Pair _cdr) {
+        return new Pair(_car, _cdr);
     }
 
-    private List cons(List _car, List _cdr) {
-        return new LList(_car, _cdr);
+    private Pair cons(Pair _car, Pair _cdr) {
+        return new Pair(_car, _cdr);
     }
 
-    private List cons(UnaryOperator<List> _car, List _cdr) {
-        return new Prim(_car);
+    private Pair cons(UnaryOperator<Pair> _car, Pair _cdr) {
+        return new Pair(_car, _cdr);
+    }
+
+    private Pair cons(Object _car, Pair _cdr) {
+        if (is_atom(_car)) return new Pair((String)_car, _cdr);
+        return new Pair((Pair)_car, _cdr);
     }
 
     private String toChars(int[] s) {
         StringBuffer ret = new StringBuffer(32);
         for (int c: s) {
+            if (c == '\0') break;
             ret.append((char)c);
         }
         return ret.toString();
     }
 
-    private List intern(int[] sym) {
+    private String intern(int[] sym) {
         return intern(toChars(sym));
     }
 
-    private List intern(String sym) {
-        List _pair = symbols;
+    private String intern(String sym) {
+        Pair _pair = symbols;
         for ( ; _pair != null ; _pair = cdr(_pair)) {
-            if (sym.equalsIgnoreCase(car(_pair).atom())) {
-                return car(_pair);
+            if (sym.equalsIgnoreCase((String) car(_pair))) {
+                return (String) car(_pair);
             }
         }
         symbols = cons(sym, symbols);
-        return car(symbols);
+        return (String) car(symbols);
     }
 
-    private List getobj() {
+    private Object getobj() {
         if (token[0] == '(') return getlist();
         return intern(token);
     }
 
-    private List getlist() {
+    private Object getlist() {
         gettoken();
         if (token[0] == ')') return null;
-        List tmp = getobj();
-        return cons(tmp, getlist());
+        Object tmp = getobj();
+        if (is_atom(tmp)) return cons((String)tmp, (Pair) getlist());
+        else return cons((Pair)tmp, (Pair) getlist());
     }
 
-    private void print_obj(List ob, boolean head_of_list) {
+    private void print_obj(Object ob, boolean head_of_list) {
         if (!is_pair(ob) ) {
-            out.print(ob != null ? car(ob).atom() : "null" );
+            out.print(ob != null ? ob.toString() : "null" );
         } else {
             if (head_of_list) out.print('(');
-            print_obj(car(ob), true);
-            if (cdr(ob) != null) {
+            print_obj(car((Pair) ob), true);
+            if (cdr((Pair) ob) != null) {
                 out.print(' ');
-                print_obj(cdr(ob), false);
+                print_obj(cdr((Pair) ob), false);
             } else out.print(')');
         }
     }
 
-    private UnaryOperator<List> fcons =     (List a) -> {  return cons(car(a), car(cdr(a)));  };
-    private UnaryOperator<List> fcar =      (List a) -> {  return car(car(a));  };
-    private UnaryOperator<List> fcdr =      (List a) -> {  return cdr(car(a));  };
-    private UnaryOperator<List> feq =       (List a) -> {  return car(a) == car(cdr(a)) ? e_true() : e_false();  };
-    private UnaryOperator<List> fpair =     (List a) -> {  return is_pair(car(a))       ? e_true() : e_false();  };
-    private UnaryOperator<List> fatom =     (List a) -> {  return is_atom(car(a))       ? e_true() : e_false();  };
-    private UnaryOperator<List> fnull =     (List a) -> {  return car(a) == null        ? e_true() : e_false(); };
-    private UnaryOperator<List> freadobj =  (List a) -> {  look = getchar(); gettoken(); return getobj();  };
-    private UnaryOperator<List> fwriteobj = (List a) -> {  print_obj(car(a), true); out.println(""); return e_true();  };
+    private UnaryOperator<Pair> fcons =     (Pair a) -> {  return cons(car(a), (Pair)car(cdr(a)));  };
+    private UnaryOperator<Pair> fcar =      (Pair a) -> {  return (Pair) car((Pair) car(a));  };
+    private UnaryOperator<Pair> fcdr =      (Pair a) -> {  return cdr((Pair) car(a));  };
+    private UnaryOperator<Pair> feq =       (Pair a) -> {  return car(a) == car(cdr(a)) ? e_true() : e_false();  };
+    private UnaryOperator<Pair> fpair =     (Pair a) -> {  return is_pair(car(a))       ? e_true() : e_false();  };
+    private UnaryOperator<Pair> fatom =     (Pair a) -> {  return is_atom(car(a))       ? e_true() : e_false();  };
+    private UnaryOperator<Pair> fnull =     (Pair a) -> {  return car(a) == null        ? e_true() : e_false(); };
+    private UnaryOperator<Pair> freadobj =  (Pair a) -> {  look = getchar(); gettoken(); return (Pair) getobj();  };
+    private UnaryOperator<Pair> fwriteobj = (Pair a) -> {  print_obj(car(a), true); out.println(""); return e_true();  };
 
-    private List evlist(List list, List env) {
+    private Pair evlist(Pair list, Pair env) {
         /* http://cslibrary.stanford.edu/105/LinkedListProblems.pdf */
-        List head = null, insertPos = null;
+        Pair head = null, insertPos = null;
         for ( ; list != null ; list = cdr(list) ) {
-            List currentArg = cons(eval(car(list), env), null);
+            Pair currentArg = cons(eval(car(list), env), null);
             if (head == null) {
                 head = currentArg;
                 insertPos = head;
             }
             else {
-                ((LList)insertPos).cdr = currentArg;
+                insertPos.cdr = currentArg;
                 insertPos = currentArg;
             }
         }
         return head;
     }
 
-    private List apply_primitive(UnaryOperator<List> primfn, List args) {
+    private Pair apply_primitive(UnaryOperator<Pair> primfn, Pair args) {
         return primfn.apply(args);
     }
 
-    private List eval(List exp, List env) {
+    private Object eval(Object exp, Pair env) {
+        //out.print("exp: "); print_obj(exp, true); out.println();
+        //out.print("env: "); print_obj(env, true); out.println();
+        //out.flush();
+
         if (is_atom(exp) ) {
             for ( ; env != null; env = cdr(env) )
-                if (exp == car(car(env)))  return car(cdr(car(env)));
+                if (exp == car((Pair) car(env)))
+                    return car(cdr((Pair) car(env)));
             return null;
-        } else if (is_atom( car (exp))) { /* special forms */
-            if (car(exp) == intern("quote")) {
-                return car(cdr(exp));
-            } else if (car(exp) == intern("if")) {
-                if (eval (car(cdr(exp)), env) != null)
-                    return eval (car(cdr(cdr(exp))), env);
+        } else if (is_atom( car ((Pair) exp))) { /* special forms */
+            if (car((Pair) exp) == intern("quote")) {
+                return car(cdr((Pair) exp));
+            } else if (car((Pair) exp) == intern("if")) {
+                if (eval (car(cdr((Pair) exp)), env) != null)
+                    return eval (car(cdr(cdr((Pair) exp))), env);
                 else
-                    return eval (car(cdr(cdr(cdr(exp)))), env);
-            } else if (car(exp) == intern("lambda")) {
+                    return eval (car(cdr(cdr(cdr((Pair) exp)))), env);
+            } else if (car((Pair) exp) == intern("lambda")) {
                 return exp; /* todo: create a closure and capture free vars */
-            } else if (car(exp) == intern("apply")) { /* apply function to list */
-                List args = evlist (cdr(cdr(exp)), env);
-                args = car(args); /* assumes one argument and that it is a list */
-                return apply_primitive( eval(car(cdr(exp)), env).prim(), args);
+            } else if (car((Pair) exp) == intern("apply")) { /* apply function to list */
+                Pair args = evlist (cdr(cdr((Pair) exp)), env);
+                args = (Pair)car(args); /* assumes one argument and that it is a list */
+                return apply_primitive( (UnaryOperator<Pair>) eval(car(cdr((Pair) exp)), env), args);
             } else { /* function call */
-                List primop = eval (car(exp), env);
+                Object primop = eval (car((Pair) exp), env);
                 if (is_pair(primop)) { /* user defined lambda, arg list eval happens in binding  below */
-                    return eval( cons(primop, cdr(exp)), env );
+                    return eval( cons(primop, cdr((Pair) exp)), env );
                 } else if (primop != null) { /* built-in primitive */
-                    return apply_primitive(primop.prim(), evlist(cdr(exp), env));
+                    return apply_primitive((UnaryOperator<Pair>) primop, evlist(cdr((Pair) exp), env));
                 }
             }
-        } else if (car(car(exp)) == intern("lambda")) { /* should be a lambda, bind names into env and eval body */
-            List extenv = env, names = car(cdr(car(exp))), vars = cdr(exp);
+        } else if (car((Pair) car((Pair) exp)) == intern("lambda")) { /* should be a lambda, bind names into env and eval body */
+            Pair extenv = env, names = (Pair) car(cdr((Pair) car((Pair) exp))), vars = cdr((Pair) exp);
             for (  ; names != null; names = cdr(names), vars = cdr(vars) )
-                extenv = cons (cons(car(names),  cons(eval (car(vars), env), null)), extenv);
-            return eval (car(cdr(cdr(car(exp)))), extenv);
+                extenv = cons (cons((String) car(names),  cons(eval (car(vars), env), null)), extenv);
+            return eval (car(cdr(cdr((Pair) car((Pair) exp)))), extenv);
         }
         out.println("cannot evaluate expression");
         return null;
@@ -212,16 +202,16 @@ public class Lambda {
     private void run(InputStream in, PrintStream out) {
         this.in = in;
         this.out = out;
-        List env = cons (cons(intern("car").atom(),     cons(fcar, null)),
-                   cons (cons(intern("cdr").atom(),     cons(fcdr, null)),
-                   cons (cons(intern("cons").atom(),    cons(fcons, null)),
-                   cons (cons(intern("eq?").atom(),     cons(feq, null)),
-                   cons (cons(intern("pair?").atom(),   cons(fpair, null)),
-                   cons (cons(intern("symbol?").atom(), cons(fatom, null)),
-                   cons (cons(intern("null?").atom(),   cons(fnull, null)),
-                   cons (cons(intern("read").atom(),    cons(freadobj, null)),
-                   cons (cons(intern("write").atom(),   cons(fwriteobj, null)),
-                   cons (cons(intern("null").atom(),    cons((String)null,null)), null))))))))));
+        Pair env = cons (cons(intern("car"),     cons(fcar, null)),
+                   cons (cons(intern("cdr"),     cons(fcdr, null)),
+                   cons (cons(intern("cons"),    cons(fcons, null)),
+                   cons (cons(intern("eq?"),     cons(feq, null)),
+                   cons (cons(intern("pair?"),   cons(fpair, null)),
+                   cons (cons(intern("symbol?"), cons(fatom, null)),
+                   cons (cons(intern("null?"),   cons(fnull, null)),
+                   cons (cons(intern("read"),    cons(freadobj, null)),
+                   cons (cons(intern("write"),   cons(fwriteobj, null)),
+                   cons (cons(intern("null"),    cons((String)null,null)), null))))))))));
         look = getchar();
         gettoken();
         print_obj( eval(getobj(), env), true );
