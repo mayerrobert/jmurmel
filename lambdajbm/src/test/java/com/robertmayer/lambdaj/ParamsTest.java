47f43e3e73f3c0f16a265ecb42e8aeb2ec366fae
Robert <Robert>
2020-10-11 10:06:14 +0000

    run eval 5 times on the same interpreter object

--- lambdajbm/src/test/java/com/robertmayer/lambdaj/ParamsTest.java
+++ lambdajbm/src/test/java/com/robertmayer/lambdaj/ParamsTest.java
@@ -15,9 +15,23 @@ public class ParamsTest {
     public void runPrograms() {
         for (int prog = 0; prog < PROGRAMS.length; prog++) {
             LambdaJ interpreter = new LambdaJ();
-            Object result = interpreter.interpretExpression(new StringReader(PROGRAMS[prog])::read, (s) -> { return; });
-            if (result == null) result = "nil";
-            assertEquals("prog " + prog, RESULTS[prog], result.toString());
+            run(prog, interpreter);
         }
+    }
+
+    // check if multiple invocations on the same interpreter object deliver correct results
+    @Test
+    public void runProgramsFiveTimes() {
+        for (int prog = 0; prog < PROGRAMS.length; prog++) {
+            LambdaJ interpreter = new LambdaJ();
+            for (int n = 0; n < 5; n++)
+                run(prog, interpreter);
+        }
+    }
+
+    private void run(int prog, LambdaJ interpreter) {
+        Object result = interpreter.interpretExpression(new StringReader(PROGRAMS[prog])::read, (s) -> { return; });
+        if (result == null) result = "nil";
+        assertEquals("prog " + prog, RESULTS[prog], result.toString());
     }
 }
