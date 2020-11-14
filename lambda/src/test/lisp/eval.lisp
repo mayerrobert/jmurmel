eb086d7e6f3645aaefe763bb939efdd672e5ff4c
Robert <Robert>
2020-11-14 15:19:22 +0000

    dok

--- lambda/src/test/lisp/eval.lisp
+++ lambda/src/test/lisp/eval.lisp
@@ -1,8 +1,8 @@
 ;;; eval implemented in LISP
 ;;; based on https://www.reddit.com/r/lisp/comments/aklz0x/what_is_the_smallest_definition_of_lisp/
 
-; needs a LISP with   S-expressions, symbols and cons, car, cdr, eq, atom, quote, cond, labels, defun
-; implements a LISP 1 (dyn env) with symbols and cons, car, cdr, eq, atom, quote, cond, label, lambda
+; needs a LISP with   S-expressions, symbols and cons, car, cdr, eq, atom, quote, cond, labels, defun and t
+; implements a LISP 1 (dyn env) with symbols and cons, car, cdr, eq, atom, quote, cond, label, lambda and t
 
 ;;; result of the test function:
 ; result: (AA BB CC DD EE FF)
@@ -14,15 +14,16 @@
 (defun caddr  (l) (car (cdr (cdr l))))
 (defun caddar (l) (car (cdr (cdr (car l)))))
 
-(defun my-list (a b) (cond ((eq  a ()) (cond ((atom b) (cons b ()))
-                                           (t b)))
-                         (t (cons a (cons b ())))))
+(defun my-list (a b) 
+  (cond ((eq  a ()) (cond ((atom b) (cons b ()))
+                          (t b)))
+        (t (cons a (cons b ())))))
 
 (defun append (a b)
-       (cond ((eq () a) b)
-              (t (cons (car a) (append (cdr a) b)))))
-              
-              
+  (cond ((eq () a) b)
+        (t (cons (car a) (append (cdr a) b)))))
+
+
 (labels
 
   ;;; eval
@@ -60,40 +61,36 @@
                          e)))))
 
    ;;; evcon
-   (evcon (c e)          
+   (evcon (c e)
      (cond ((my-eval (caar c) e)
-             (my-eval (cadar c) e))
-           (t      
-             (evcon (cdr c) e))))
+              (my-eval (cadar c) e))
+           (t (evcon (cdr c) e))))
 
    ;;; evlis
    (evlis (a e)
-     (cond ((eq () a)
-             ())          
-           (t      
-             (cons (my-eval (car a) e) 
-                   (evlis (cdr a) e)))))
+     (cond ((eq () a) ())
+           (t (cons (my-eval (car a) e)
+                    (evlis (cdr a) e)))))
 
    ;;; assoc
-   (assoc (x a)      
-     (cond ((eq () a) ())      
-           ((eq x (caar a)) (cadar a))  
+   (assoc (x a)
+     (cond ((eq () a) ())
+           ((eq x (caar a)) (cadar a))
            (t (assoc x (cdr a)))))
 
    ;;; pair
    (pair (a b)
      (cond ((eq () a) ())
            ((eq () b) ())
-           (t
-             (cons (list (car a) (car b))
-                   (pair (cdr a) (cdr b)))))))
-				   
+           (t (cons (list (car a) (car b))
+                    (pair (cdr a) (cdr b)))))))
+
   ;;; test eval: use the provided append to append two lists
-  (my-eval (quote ((label append  
-                  (lambda (a b)
-                    (cond ((eq () a) b)
-                          (t (cons (car a)
-                                   (append (cdr a) b))))))
+  (my-eval (quote ((label append
+                     (lambda (a b)
+                       (cond ((eq () a) b)
+                             (t (cons (car a)
+                                      (append (cdr a) b))))))
                 (quote (AA BB CC))  
                 (quote (DD EE FF))))
-        (quote ((t t)))))
+           (quote ((t t)))))
