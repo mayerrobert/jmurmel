0cc22fb79176e58ab48c3d1bc4abe6a21a4ed91e
Robert <Robert>
2020-11-26 17:36:13 +0000

    CPS experiment

---
+++ lambda/src/test/java/com/robertmayer/lambdaj/recurs/Factorial.java
@@ -0,0 +1,45 @@
+package com.robertmayer.lambdaj.recurs;
+
+import java.math.BigInteger;
+
+/**
+ * Factorial CPS-style implemented with manual trampolining
+ *
+ * see https://stackoverflow.com/questions/189725/what-is-a-trampoline-function
+ */
+class Trampoline<T> {
+    public T get() { return null; }
+    public Trampoline<T>  run() { return null; }
+
+    T execute() {
+        Trampoline<T>  trampoline = this;
+
+        while (trampoline.get() == null) {
+            trampoline = trampoline.run();
+        }
+
+        return trampoline.get();
+    }
+}
+
+public class Factorial {
+    public static Trampoline<BigInteger> factorial(final int n, final BigInteger product) {
+        if(n <= 1) {
+            return new Trampoline<BigInteger>() { @Override
+            public BigInteger get() { return product; } };
+        }
+        else {
+            return new Trampoline<BigInteger>() {
+                @Override
+                public Trampoline<BigInteger> run() {
+                    return factorial(n - 1, product.multiply(BigInteger.valueOf(n)));
+                }
+            };
+        }
+    }
+
+    public static void main( String [ ] args ) {
+        final BigInteger result = factorial(100, BigInteger.ONE).execute();
+        System.out.println(result);
+    }
+}
