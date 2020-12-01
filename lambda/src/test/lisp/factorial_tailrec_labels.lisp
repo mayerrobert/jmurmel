bdd827994e6ce38b488f8d8e55fec011640e209a
Robert <Robert>
2020-12-01 19:41:11 +0000

    dok

--- lambda/src/test/lisp/factorial_tailrec_labels.lisp
+++ lambda/src/test/lisp/factorial_tailrec_labels.lisp
@@ -6,7 +6,7 @@
 (labels ((factTR (n a)
                  (labels ((mult (lhs rhs) (* lhs rhs)))
                    (cond ((= n 0) a)
-                       (t (factTR (- n 1) (* n a)))))))
+                       (t (factTR (- n 1) (mult n a)))))))
 
 ; A wrapper over factTR 
  (factTR 4 1))
