01dddea9a09597299645fdfc80031e60e3dfbf40
Robert <Robert>
2020-12-12 10:55:51 +0000

    more TCO experiments

--- lambda/src/test/java/com/robertmayer/lambdaj/recurs/CPSManualTest.java
+++ lambda/src/test/java/com/robertmayer/lambdaj/recurs/CPSManualTest.java
@@ -8,42 +8,49 @@ import com.robertmayer.lambdaj.LambdaJ.LambdaJError;
 
 public class CPSManualTest {
 
-    interface MurmelCPSFunction { Result apply(Object... args) throws LambdaJError; }
+    interface MurmelCPSFunction { MurmelCPSResult apply(Object... args) throws LambdaJError; }
 
-    static class Result {
+    static final MurmelCPSFunction EARLY_RETURN = null;
+
+    static class MurmelCPSResult {
         Object result;
         MurmelCPSFunction next;
         Object[] args;
 
-        Result() {}
-        Result(Object result, MurmelCPSFunction next, Object[] args) {
+        MurmelCPSResult(Object result, MurmelCPSFunction next, Object[] args) {
             this.result = result;
             this.next = next;
             this.args = args;
         }
     }
 
-    static Result makeResult(Object result) {
-        return new Result(result, null, null);
-    }
-
-    static Result makeEarlyReturn(Object result, MurmelCPSFunction next) {
-        return new Result(result, next, null);
-    }
-
-    static Result makeTailCall(MurmelCPSFunction func, Object... args) {
-        return new Result(null, func, args);
-    }
-
-    Object apply(MurmelCPSFunction f, Object... args) {
-        Result r = new Result();
-        r.next = f;
-        r.args = args;
+    private static Object funcall(MurmelCPSFunction f, Object... args) {
+        MurmelCPSResult r = new MurmelCPSResult(null, f, args);
         do {
             r = r.next.apply(r.args);
         } while (r.next != null);
         return r.result;
     }
+
+    private static MurmelCPSResult makeResult(Object result) {
+        return new MurmelCPSResult(result, null, null);
+    }
+
+    // das koennt gehen, fuer (return form), fuer (return-from name form...) ists aber zuwenig
+    private static MurmelCPSResult makeEarlyReturn(Object result) {
+        return new MurmelCPSResult(result, EARLY_RETURN, null);
+    }
+
+    private static MurmelCPSResult makeTailCall(MurmelCPSFunction func, Object... args) {
+        return new MurmelCPSResult(null, func, args);
+    }
+
+    // das geht so nicht, weil eine continuation ist mehr, hat state
+    private static MurmelCPSResult makeContCall(Object result, MurmelCPSFunction next, Object... args) {
+        return new MurmelCPSResult(result, next, args);
+    }
+
+    private static double dbl(Object o) { return ((Number)o).doubleValue(); }
 
 
 
@@ -64,7 +71,7 @@ public class CPSManualTest {
 
      */
 
-    Result f1(Object... args) {
+    MurmelCPSResult f1(Object... args) {
         Object a = args[0]; Object n = args[1];
         if (a == n) {
             return makeResult(a);
@@ -74,11 +81,11 @@ public class CPSManualTest {
         }
     }
 
-    Result f2(Object... args) {
+    MurmelCPSResult f2(Object... args) {
         return makeResult(null);
     }
 
-    Result f3(Object... args) {
+    MurmelCPSResult f3(Object... args) {
         return makeResult(null);
     }
 
@@ -86,15 +93,14 @@ public class CPSManualTest {
     public void testCps() {
         Object result;
 
-        result = apply(this::f2);
-        result = apply(this::f3);
-        result = apply(this::f1, new Object[] { 0, 5 });
+        result = funcall(this::f2);
+        result = funcall(this::f3);
+        result = funcall(this::f1, new Object[] { 0, 5 });
 
         assertEquals(Integer.valueOf(5), result);
     }
 
 
-    double dbl(Object o) { return ((Number)o).doubleValue(); }
 
     /*
     (defun factorial (n product)
@@ -103,17 +109,54 @@ public class CPSManualTest {
         (factorial (- n 1) (* product n))))
 
     (factorial 5 1) ; ==> 120
-   */
-    Result factorial(Object... args) {
+    */
+    MurmelCPSResult factorial(Object... args) {
         Object n = args[0];
         Object product = args[1];
-        if (dbl(n) <= 1) return makeResult(product);
-        else return makeTailCall(this::factorial, dbl(n) - 1, dbl(product) * dbl(n));
+        MurmelCPSResult result = null;
+
+        result = (dbl(n) <= 1)
+        ? makeResult(product)
+        : makeTailCall(this::factorial, dbl(n) - 1, dbl(product) * dbl(n));
+        if (result.next != null) return result;
+
+        return result;
     }
 
     @Test
     public void testFac() {
-        Object res = apply(this::factorial, 5, 1);
+        Object res = funcall(this::factorial, 5, 1);
         assertEquals(120.0, dbl(res), 1e-35);
+    }
+
+
+
+    /*
+    (defun ackermann (m n)
+      (cond
+        ((= m 0) (+ n 1))
+        ((= n 0) (ackermann (- m 1) 1))
+        (t (ackermann (- m 1) (ackermann m (- n 1))))))
+
+    (ackermann 3 6) ; ==> 509
+     */
+    MurmelCPSResult ackermann(Object... args) {
+        Object m = args[0];
+        Object n = args[1];
+        MurmelCPSResult result = null;
+
+        result = false ? null
+        : (dbl(m) == 0.0) ? makeResult(dbl(n) + 1)
+        : (dbl(n) == 0.0) ? makeTailCall(this::ackermann, dbl(m) - 1.0, 1.0)
+        : makeTailCall(this::ackermann, dbl(m) - 1, funcall(this::ackermann, m, dbl(n) - 1.0));
+        if (result.next != null) return result;
+
+        return result;
+    }
+
+    @Test
+    public void testAckermann() {
+        Object res = funcall(this::ackermann, 3.0, 6.0);
+        assertEquals(509.0, dbl(res), 1e-35);
     }
 }
