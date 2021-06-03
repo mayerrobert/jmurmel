3a86a82ee9e269daa148354bcd00f4b5cb92a7e6
Robert Mayer <mayer_robert@gmx.net>
2021-06-03 21:20:14 +0200

    added test with closures

---
+++ lambda/src/test/lisp/twoclosures.lisp
@@ -0,0 +1,11 @@
+; output: 1.02.03.02.01.00.0-1.00.0
+; result: t
+
+; inc and dec close over the same variable n
+(let ((n 0))
+  (defun inc ()
+    (write (setq n (1+ n))))
+  (defun dec ()
+    (write (setq n (1- n)))))
+
+(inc) (inc) (inc) (dec) (dec) (dec) (dec) (inc)
