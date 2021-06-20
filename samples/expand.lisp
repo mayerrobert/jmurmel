0c083dbcb5c7e9cfad9eb9f8ec7839df5cc3debe
Robert Mayer <mayer_robert@gmx.net>
2021-06-20 21:26:59 +0200

    sample for expanding macros

---
+++ samples/expand.lisp
@@ -0,0 +1,31 @@
+;;; Expand macros inside a form. Only macros defined
+;;; before the form will be expanded,
+;;; defmacro calls within the form are not considered.
+
+(require "mlib")
+
+(defun m%macroexpand-1 (ex)
+  (let ((expanded (macroexpand-1 ex)))
+    (if (eq ex expanded)
+          (list ex nil)
+      (list expanded t))))
+
+(defun expand (form)
+  (cond ((atom form)
+         form)
+
+        ((eq 'quote (car form))
+         'form)
+
+        (t
+         (let* ((expanded-args (let loop ((l (cdr form)))
+                                 (if (atom l)
+                                       l
+                                   (cons (expand (car l)) (loop (cdr l))))))
+                (form-expanded-args (cons (car form) expanded-args)))
+           (destructuring-bind
+             (ex changed)
+             (m%macroexpand-1 form-expanded-args)
+             (if changed
+                   (expand ex)
+               ex))))))
