233ca87a0073f8746523b443a28bede40a63fc11
Robert Mayer <mayer_robert@gmx.net>
2022-02-28 06:59:28 +0100

    benchmark results
    checkin benchmark results in order to keep track of changes in performance

--- samples.mlib/benchmark/runall.cmd
+++ samples.mlib/benchmark/runall.cmd
@@ -1,5 +1,14 @@
 @echo off
 
 REM run all benchmarks with sbcl and jmurmel
+REM Usage: runall | tee benchresults.txt
 
-for %%i in (00*.lisp 3*.lisp q.lisp) do @echo. & @echo *** sbcl: %%i *** & @sbcl --script %%i & @echo. & @echo *** jmurmel: %%i *** & @java -jar ..\..\lambda\target\jmurmel.jar --run --libdir .. %%i
+set SBCL=c:\apps\sbcl\2.2.0\bin\sbcl.exe
+set JMURMEL=java -jar ..\..\lambda\target\jmurmel.jar --libdir ..
+
+echo Benchmark results
+date /t
+%SBCL% --version
+%JMURMEL% --version
+
+for %%i in (00*.lisp 3*.lisp q.lisp) do @echo. & @echo *** sbcl: %%i *** & @%SBCL% --script %%i & @echo. & @echo *** jmurmel: %%i *** & @%JMURMEL% --run %%i
