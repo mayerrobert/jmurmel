11ffc05d906d7545bd6951bfa7dfaca09252e1b5
Robert Mayer <mayer_robert@gmx.net>
2022-03-30 06:38:37 +0200

    cleanup bitmap sample

--- samples.murmel/bitmap.lisp
+++ samples.murmel/bitmap.lisp
@@ -1,23 +1,20 @@
-;;; create a window with a bitmap and fill the bitmap with all colors, brightness is fixed at 1
+;;;; Demo for using a bitmap and setting pixels to an HSB color value
+;;;;
+;;;; create a window with a bitmap and fill the bitmap with all colors, brightness is fixed at 1
 
-(define W 800)
-(define H 600)
+(define *width* 800)
+(define *height* 600)
 
-;(define wlog 720) ; account for 40px default padding
-;(define hlog 520) ; account for 40px default padding
-(define wlog W)
-(define hlog H)
-
-(make-frame "HSB" W H 0) ; frame with zero padding
-(make-bitmap wlog hlog)  ; associate bitmap with frame so that set-pixel can be used
+(make-frame "HSB Bitmap Demo" *width* *height* 0) ; frame with zero padding
+(make-bitmap *width* *height*)  ; associate bitmap with frame so that set-pixel can be used
 
 (let xloop ((x 0))
-  (let ((hue (/ x wlog)))
+  (let ((hue (/ x *width*)))
     (let yloop ((y 0))
-      (set-pixel x y (hsb-to-pixel hue (/ y hlog) 1))
-      (if (< y (- hlog 1))
-        (yloop (+ y 1)))))
-  (if (< x (- wlog 1))
-    (xloop (+ x 1))))
+      (set-pixel x y (hsb-to-pixel hue (/ y *height*) 1))
+      (if (< y (- *height* 1))
+        (yloop (1+ y)))))
+  (if (< x (- *width* 1))
+    (xloop (1+ x))))
 
 (open-frame)
