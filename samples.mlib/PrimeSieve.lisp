adc406cc6f8eeee0fc74477ca34ce4b743b3ec61
Robert Mayer <mayer_robert@gmx.net>
2022-08-09 20:02:04 +0200

    add PrimeSieve expample

---
+++ samples.mlib/PrimeSieve.lisp
@@ -0,0 +1,97 @@
+;;;; Murmel solution for Dave Plummer's Software Drag Race,
+;;;; loosely based on mikehw's Common Lisp solution
+;;;
+;;; run as:
+;;;     java -jar jmurmel.jar --run PrimeSieve.lisp
+;;; or
+;;;     sbcl --script PrimeSieve.lisp
+;;; or
+;;;     abcl --batch --load PrimeSieve.lisp
+;;;
+;;; see https://github.com/PlummersSoftwareLLC/Primes
+;;; see https://github.com/mayerrobert/jmurmel
+
+
+#+murmel (require "mlib")
+#-murmel (defmacro define (s v) `(defparameter ,s ,v))
+
+
+(define *rawbits* ())
+(define *sieve-size* 0)
+
+
+(defun prime-sieve (limit)
+  (setq *sieve-size*  limit)
+  (setq *rawbits* (make-array (ceiling limit 2)))
+
+  (dotimes (i (length *rawbits*))
+    (setf (svref *rawbits* i) 1)))
+
+
+(defmacro get-bit (rawbits index)
+  `(svref ,rawbits (floor ,index 2)))
+
+
+(defmacro clear-bit (rawbits index)
+  `(setf (svref ,rawbits (floor ,index 2)) 0))
+
+
+(defun run-sieve ()
+  (do* ((rawbits *rawbits*)
+        (factor 3)
+        (sieve-size *sieve-size*)
+        (q (floor (sqrt sieve-size))))
+       ((>= factor q))
+
+    (do ((num factor))
+        ((or (>= num sieve-size)
+             (= (get-bit rawbits num) 1))
+         (setq factor num))
+        (incf num 2))
+
+    (do ((num (* factor 3))
+         (increment (* factor 2)))
+        ((>= num sieve-size))
+
+        (clear-bit rawbits num)
+        (incf num increment))
+
+    (incf factor 2)))
+
+
+(defun count-primes ()
+  (reduce #'+ *rawbits*))
+
+
+(define *passes* 0)
+(define *start* (get-internal-real-time))
+
+
+(do ((runtime (* internal-time-units-per-second 5)))
+    ((> (- (get-internal-real-time) *start*) runtime))
+
+  (prime-sieve 1000000)
+  (run-sieve)
+  (incf *passes*))
+
+
+(define *duration* (/ (- (get-internal-real-time) *start*) internal-time-units-per-second))
+(define *avg* (/ *duration* *passes*))
+
+
+#|
+(princ "2, ")
+(do ((n 3))
+    ((>= n 100))
+  (when (= 1 (get-bit *rawbits* n))
+    (write n) (princ ", "))
+  (setq n (1+ (1+ n))))
+(terpri)
+|#
+
+
+#+murmel (format t "passes: %d, count: %d, avg: %f%n" *passes* (floor (count-primes)) *avg*)
+#+murmel (format t "murmel;%d;%f;1;algorithm=base,faithful=no,bits=unknown%n" *passes* *duration*)
+
+#-murmel (format t "passes: ~d, count: ~d, avg: ~f~%" *passes* (count-primes) *avg*)
+#-murmel (format t "sbcl;~d;~f;1;algorithm=base,faithful=no,bits=unknown~%" *passes* *duration*)
