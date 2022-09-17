212910d08a7fdd0bdfa2a01bc7452e0539e77d40
Robert Mayer <mayer_robert@gmx.net>
2022-09-17 16:02:11 +0200

    added cmd file to run benchmarks with sbcl

---
+++ samples.mlib/benchmark/runsbcl.cmd
@@ -0,0 +1,23 @@
+@echo off
+
+REM run all benchmarks with sbcl
+REM Usage: runsbcl | tee sbclresults.txt
+
+REM config
+set SBCL=sbcl
+REM end config
+
+
+echo Benchmark results
+date /t
+echo.
+
+%SBCL% --version
+
+for %%i in (00*.lisp 3*.lisp a*.lisp q.lisp) do call :all %%i
+goto :EOF
+
+:all
+echo.
+echo *** sbcl: %1 ***
+%SBCL% --script %1
