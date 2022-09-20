9dc9670349d49b92c55d12a314803f9c48f20ebc
Robert Mayer <mayer_robert@gmx.net>
2022-09-20 11:15:06 +0200

    port to Murmel
    remove type declarations
    replace loop by dotimes and do
    replace aref by svref

--- samples.mlib/benchmark/fannkuch.lisp
+++ samples.mlib/benchmark/fannkuch.lisp
@@ -1,53 +1,53 @@
 ;;;; Fannkuch-Redux
 ;;;;
-;;;; Slightly modified from https://benchmarksgame-team.pages.debian.net/benchmarksgame/program/fannkuchredux-sbcl-2.html
+;;;; Modified from https://benchmarksgame-team.pages.debian.net/benchmarksgame/program/fannkuchredux-sbcl-2.html
 ;;;; which is licensed under the "3-Clause BSD License" https://benchmarksgame-team.pages.debian.net/benchmarksgame/license.html
 
-(defun fannkuch (n)
-  (declare (type fixnum n))
-  (let ((csum 0)
-	(fmax 0))
-    (declare (type fixnum fmax))
-    (let ((perm (make-array n :element-type 'fixnum))
-	  (copy (make-array n :element-type 'fixnum))
-	  (num 0)) 
+#+murmel (load "mlib")
 
-      (loop for i from 0 to (- n 1) do (setf (aref perm i) i))
+(defun fannkuch (n)
+  (let ((csum 0)
+        (fmax 0)
+        (perm (make-array n ;:element-type 'fixnum
+                          ))
+        (copy (make-array n ;:element-type 'fixnum
+                          ))
+        (num 0))
+
+      (dotimes (i n) (setf (svref perm i) i))
 
       (labels ((do-iter (ht)
-		 
-		 (declare (type fixnum ht))
-		 
-		 (if (= ht 1)
-		     (progn
-		       (loop for i from 0 to (- n 1) do (setf (aref copy i) (aref perm i)))
-		       (let ((c 0))
-			 (declare (type fixnum c))
-			 (let ((z (aref copy 0)))
-			   (loop until (= z 0) do
-				(progn
-				  (loop for i from 0 to (ash z -1)
-				     do (let ((temp (aref copy i))
-					      (k (- z i)))
-					  (setf (aref copy i) (aref copy k))
-					  (setf (aref copy k) temp)))
-				  (incf c)
-				  (setf z (aref copy 0)))))
-			 (setf csum (+ csum  (if (evenp num) c (- c))))
-			 (when (> c fmax)
-			   (setf fmax c)))
-		       (incf num))
-		     (loop for i from 1 to ht do
-			  (progn (do-iter (- ht 1))
-				 (let ((temp (aref perm 0))
-				       (m (- ht 1)))
-				   (loop for i from 1 to m do
-					(setf (aref perm (- i 1)) (aref perm i)))
-				   (setf (aref perm m) temp)))))))
+                 (if (= ht 1)
+                     (progn
+                       (dotimes (i n) (setf (svref copy i) (svref perm i)))
+                       (let ((c 0)
+                             (z (svref copy 0)))
+                         (do () ((= z 0))
+                           (progn
+                             (dotimes (i (1+ (truncate z 2)))
+                               (let ((temp (svref copy i))
+                                     (k (- z i)))
+                                 (setf (svref copy i) (svref copy k))
+                                 (setf (svref copy k) temp)))
+                             (incf c)
+                             (setf z (svref copy 0))))
+                         (setf csum (+ csum  (if (evenp num) c (- c))))
+                         (when (> c fmax)
+                           (setf fmax c)))
+                       (incf num))
+                     (dotimes (i ht)
+                       (progn (do-iter (- ht 1))
+                              (let ((temp (svref perm 0))
+                                    (m (- ht 1)))
+                                (dotimes (i m)
+                                  (setf (svref perm i) (svref perm (1+ i))))
+                                (setf (svref perm m) temp)))))))
 
-	(do-iter n)))
-    (format t "~s~%Pfannkuchen(~s) = ~s~%" csum n fmax)))
+        (do-iter n))
+      #-murmel (format t "~s~%Pfannkuchen(~s) = ~s~%" csum n fmax)
+      #+murmel (format t "%s%nPfannkuchen(%s) = %s%n" csum n fmax)
+      ))
 
 
-;(fannkuch 12) ; takes approx a minute
-(time (fannkuch 10)) ; takes approx 0.2 seconds
+;(fannkuch 12) ; takes approx a minute on SBCL, checksum 3968050
+(time (fannkuch 10)) ; takes < 1 second on SBCL, checksum 73196
