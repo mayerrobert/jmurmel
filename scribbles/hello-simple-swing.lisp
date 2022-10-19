351cb7c4b4da83920757084fb303b4d2392ae56f
Robert Mayer <mayer_robert@gmx.net>
2022-10-19 07:10:05 +0200

    swing sample using messagebox

---
+++ scribbles/hello-simple-swing.lisp
@@ -0,0 +1,8 @@
+;;;; Super-simple "Hello, World!" using a Swing messagebox
+;;;
+;;; This will open a messagebox and block until the OK button is clicked.
+;;; Then print "Bye." and exit.
+
+((jmethod "javax.swing.JOptionPane" "showMessageDialog" "java.awt.Component" "Object") nil "Hello, World!")
+
+(writeln "Bye." nil)
