1952cc0758e21af2051dd0e070dda463eba6d829
Robert Mayer <mayer_robert@gmx.net>
2023-05-01 15:53:25 +0200

    smaller GraalVM executables step 2

--- graalvm/jmurmel-native.sh
+++ graalvm/jmurmel-native.sh
@@ -5,18 +5,19 @@
 # (probably and/ or Windows and/ or MacOS executables but only Linux was tested)
 #
 # Needs a local installation of GraalVM:
+# install a C compiler that will be used by GraalVM's native_image,
 # download an appropriate GraalVM distribution
 # e.g. from https://github.com/graalvm/graalvm-ce-builds/releases/tag/vm-22.3.1
 # unzip it somewhere, adjust GRAAL_HOME accordingly and then do
 #
 # $ sudo $GRAAL_HOME/bin/gu install native-image
 #
-# $ mvn install
-# $ mvn clean install -f graalvm/pom.xml
-# $ cd scripts
+# $ mvn package -pl lambda
+# $ mvn package -f graalvm/pom.xml
+# $ cd graalvm
 # $ sh jmurmel-native.sh
 #
-# This will create scripts/target/jmurmel.
+# This will create graalvm/target/jmurmel.
 # The generated jmurmel executable won't support graphics,
 # won't be able to run compiled code, i.e. ":r" won't work,
 # "jmethod" is limited to the classes listed in the file "reflectconfig",
@@ -34,11 +35,11 @@ test -d target || mkdir target
 
 native-image \
   -H:IncludeResources="META-INF/.*" \
-  -H:ReflectionConfigurationFiles=../lambda/src/main/graalvm/reflectconfig \
-  -H:DynamicProxyConfigurationFiles=../lambda/src/main/graalvm/proxyconfig \
+  -H:ReflectionConfigurationFiles=./src/main/graalvm/reflectconfig \
+  -H:DynamicProxyConfigurationFiles=./src/main/graalvm/proxyconfig \
   --no-fallback \
   --report-unsupported-elements-at-runtime \
-  -cp ../graalvm/target/unsupported.jar \
+  -cp ./target/unsupported.jar \
   -jar ../lambda/target/jmurmel.jar \
   -o target/jmurmel
 
