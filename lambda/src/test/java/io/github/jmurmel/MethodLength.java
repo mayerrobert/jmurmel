10e6018f48b688fc9aefaafa6e2d464a80b26a1b
Robert Mayer <mayer_robert@gmx.net>
2023-11-04 07:10:50 +0100

    some changes to the testprog to display method lengths

--- lambda/src/test/java/io/github/jmurmel/MethodLength.java
+++ lambda/src/test/java/io/github/jmurmel/MethodLength.java
@@ -1,36 +1,73 @@
 package io.github.jmurmel;
 
-// print method lengths
+// print method lengths and some other class information
 // see https://stackoverflow.com/questions/48338337/how-to-check-bytecode-length-of-java-method/48343150#48343150
 
+import javassist.bytecode.AccessFlag;
 import javassist.bytecode.ClassFile;
 import javassist.bytecode.CodeAttribute;
 import javassist.bytecode.MethodInfo;
 
 import java.io.DataInputStream;
 import java.io.InputStream;
+import java.util.Objects;
 
-public class MethodLength {
-    
+public final class MethodLength {
+
+    private MethodLength() {}
+
     public static void main(String[] args) throws Exception {
         pr(LambdaJ.class);
         pr(LambdaJ.SExpressionReader.class);
+        pr(LambdaJ.Chk.class);
         pr(LambdaJ.Subr.class);
         pr(LambdaJ.MurmelJavaProgram.class);
     }
-    
+
     private static void pr(Class<?> clazz) throws Exception {
         System.out.println(clazz.getName() + ": **********");
-        final String fileName = clazz.getName().replaceFirst("io.github.jmurmel.", "") + ".class";
+        final String fileName = clazz.getName().substring("io.github.jmurmel.".length()) + ".class";
 
-        try (InputStream is = clazz.getResourceAsStream(fileName)) {
+        try (InputStream is = Objects.requireNonNull(clazz.getResourceAsStream(fileName), "cannot open class file")) {
             final ClassFile cf = new ClassFile(new DataInputStream(is));
-            for(MethodInfo mi: cf.getMethods()) {
+
+            System.out.println(cf.getMethods().size() + " methods total");
+            System.out.println(cf.getMethods().stream()
+                                 .filter(f -> (f.getAccessFlags() & AccessFlag.SYNTHETIC) != 0)
+                                 .count()
+                               + " synthetic static or instance methods");
+            System.out.println(cf.getMethods().stream()
+                                 .filter(f -> (f.getAccessFlags() & AccessFlag.SYNTHETIC) == 0)
+                                 .filter(f -> (f.getAccessFlags() & AccessFlag.STATIC) != 0)
+                                 .count()
+                               + " non-synthetic static methods");
+            System.out.println(cf.getMethods().stream()
+                                 .filter(f -> (f.getAccessFlags() & AccessFlag.SYNTHETIC) == 0)
+                                 .filter(f -> (f.getAccessFlags() & AccessFlag.STATIC) == 0)
+                                 .count()
+                               + " non-synthetic instance methods");
+
+            System.out.println(cf.getFields().size() + " fields total");
+            System.out.println(cf.getFields().stream()
+                                 .filter(f -> (f.getAccessFlags() & AccessFlag.SYNTHETIC) == 0)
+                                 .filter(f -> (f.getAccessFlags() & AccessFlag.STATIC) != 0)
+                                 .count()
+                               + " non-synthetic static fields");
+            System.out.println(cf.getFields().stream()
+                                 .filter(f -> (f.getAccessFlags() & AccessFlag.SYNTHETIC) == 0)
+                                 .filter(f -> (f.getAccessFlags() & AccessFlag.STATIC) == 0)
+                                 .count()
+                               + " non-synthetic instance fields");
+
+            System.out.println(cf.getConstPool().getSize() + " constant pool entries");
+
+            System.out.println();
+
+            for (MethodInfo mi: cf.getMethods()) {
                 final CodeAttribute ca = mi.getCodeAttribute();
                 if (ca == null) continue; // abstract or native
                 final int bLen = ca.getCode().length;
-                if ((bLen >= 35 && bLen <= 39) || bLen >= 325)
-                    System.out.println(mi.getName() + " " + mi.getDescriptor() + ": " + bLen + " bytes");
+                if ((bLen > 35 && bLen <= 38) || bLen > 325) System.out.println(mi.getName() + " " + mi.getDescriptor() + ": " + bLen + " bytes, max stack = " + ca.getMaxStack());
             }
         }
         System.out.println();
