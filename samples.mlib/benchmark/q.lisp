e561abcc84562098f130a51074153ae013f7c36d
Robert Mayer <mayer_robert@gmx.net>
2023-12-19 08:22:22 +0100

    benchmark q.lisp: add some type declarations to help sbcl

--- samples.mlib/benchmark/q.lisp
+++ samples.mlib/benchmark/q.lisp
@@ -1,11 +1,15 @@
 ;;; from http://www.ulisp.com/show?1EO1
 
 #+murmel (require "mlib")
-(require "bench" "bench.lisp")
+#+murmel (require "bench" "bench.lisp")
+#-murmel
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (require "bench" "bench.lisp"))
 
 
 ; Hofstadter Q sequence
 ; From Douglas Hofstadter's book "Goedel, Escher, Bach: an Eternal Golden Braid":
+(declaim (ftype (function (fixnum) fixnum) q))  ; ignored by JMurmel but helps sbcl
 (defun q (n)
   (if (<= n 2) 1
     (+
@@ -16,6 +20,7 @@
 
 
 ; Two-dimensional recursive function Q2
+(declaim (ftype (function (fixnum fixnum) fixnum) q2))
 (defun q2 (x y)
   (if (or (< x 1) (< y 1)) 1
     (+ (q2 (- x (q2 (1- x) y)) y)
@@ -26,10 +31,11 @@
 
 ; Same as Q2 but faster because Mlib's "or" is inefficient because it expands into a let
 ; (and/ or jmurmel's let is inefficient)
+(declaim (ftype (function (fixnum fixnum) fixnum) q2*))
 (defun q2* (x y)
   (if (< x 1) 1
     (if (< y 1) 1
       (+ (q2* (- x (q2* (1- x) y)) y)
          (q2* x (- y (q2* x (1- y))))))))
 
-(bench "q2*" (q2* 7 8) *default-duration*) ; ==> 31 
+(bench "q2*" (q2* 7 8) *default-duration*) ; ==> 31
