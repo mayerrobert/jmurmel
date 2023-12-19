e8803da053956fcfa8883ec018da666981b0516d
Robert Mayer <mayer_robert@gmx.net>
2023-12-19 12:53:46 +0100

    change an old test that didn't make much sense anymore

--- lambda/src/test/lisp/letrec.lisp
+++ lambda/src/test/lisp/letrec.lisp
@@ -1,16 +1,17 @@
 ; output: "f""globval"4.0"f""globval"3.0"f""globval"2.0"f""globval"1.0"body 1""body 2"
 ; result: "body 2"
 
+(define *global* "globval")
+
 (letrec ((x (+ 1 1))
          (y (+ x 2))
          (f (lambda (n)
                     (if (= n 0)
                         nil
                         (progn (write "f")
-                               (write *global*) ; global environment is dynamic
+                               (write *global*)
                                (write n)
                                (f (- n 1)))))))
-        (define *global* "globval")
         (f y)
         (write "body 1")
         (write "body 2"))
