84a44813b114f5a3e6bb563666880702b9fe9ac8
Robert Mayer <mayer_robert@gmx.net>
2024-01-21 18:13:46 +0100

    add a reader benchmark

--- lambdajbm/src/main/java/io/github/jmurmel/GabrielBenchmark.java
+++ lambdajbm/src/main/java/io/github/jmurmel/GabrielBenchmark.java
@@ -1,6 +1,7 @@
 package io.github.jmurmel;
 
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
 
 import java.io.Reader;
 import java.io.StringReader;
@@ -34,8 +35,20 @@ public class GabrielBenchmark {
     public static final int WARMUP = 10;     // default: 5
     public static final int ITERATIONS = 5;  // default: 10
 
+    private static final String[] ALL_FILES = {
+    "all.lisp",
+    "000_lib",
+    "00_tak_apo",
+    "3.01_tak.lisp",
+    "3.01_takeuchi",
+    "3.02_stak",
+    "3.04_takl",
+    "3.10_deriv",
+    };
+
     private static Path prog;
-    private static String source;
+    private static String source, allSourceFiles;
+
     private static LambdaJ.MurmelProgram compiled;
 
     private static LambdaJ interpreter;
@@ -46,6 +59,13 @@ public class GabrielBenchmark {
         prog = Paths.get("../samples.murmel/gabriel/all.lisp");
         source = JavaUtil.readString(prog, StandardCharsets.UTF_8);
 
+        final StringBuilder sb = new StringBuilder();
+        for (String fileName: ALL_FILES) {
+            final Path p = Paths.get("../samples.murmel/gabriel/" + fileName);
+            sb.append(JavaUtil.readString(prog, StandardCharsets.UTF_8));
+        }
+        allSourceFiles = sb.toString();
+
         final LambdaJ.MurmelJavaCompiler c = new LambdaJ.MurmelJavaCompiler(null, null, LambdaJBenchmark.getTmpDir());
         final Reader reader = new StringReader(source);
         final LambdaJ.ObjectReader parser = new LambdaJ.SExpressionReader(reader::read, c.getSymbolTable(), null, prog);
@@ -53,6 +73,15 @@ public class GabrielBenchmark {
         compiled =  murmelClass.getDeclaredConstructor().newInstance();
 
         interpreter = new LambdaJ();
+    }
+
+    @Benchmark
+    public void read(Blackhole bh) {
+        final Reader reader = new StringReader(allSourceFiles);
+        final LambdaJ.ObjectReader parser = interpreter.makeReader(reader::read, prog);
+        final Object eof = new Object();
+        Object form;
+        while (eof != (form = parser.readObj(true, eof))) bh.consume(form);
     }
 
     @Benchmark
