36bb84ac43f0f001bd5927bc9833b9a24f929ea2
Robert Mayer <mayer_robert@gmx.net>
2024-01-25 18:37:48 +0100

    revert make Gabriel Murmel-samples run as CL, load appropriate files instead

--- samples.murmel/gabriel/3.02_stak.lisp
+++ samples.murmel/gabriel/3.02_stak.lisp
@@ -7,22 +7,22 @@
 (define z)
 
 (defun stak (x y z)
-  (let* #+murmel dynamic ((x x) (y y) (z z))
+  (let* dynamic ((x x) (y y) (z z))
     (stak-aux)))
 
 (defun stak-aux ()
   (if (not (< y x))
         z
-    (let #+murmel dynamic
-      ((x (let #+murmel dynamic ((x (1- x))
+    (let dynamic
+      ((x (let dynamic ((x (1- x))
                 (y y)
                 (z z))
             (stak-aux)))
-       (y (let #+murmel dynamic ((x (1- y))
+       (y (let dynamic ((x (1- y))
                 (y z)
                 (z x))
             (stak-aux)))
-       (z (let #+murmel dynamic ((x (1- z))
+       (z (let dynamic ((x (1- z))
                 (y x)
                 (z y))
             (stak-aux))))
--- samples.murmel/gabriel/all.lisp
+++ samples.murmel/gabriel/all.lisp
@@ -3,14 +3,15 @@
 ;;; See also the JMH benchmark lambdajbm/src/main/io/github/jmurmel/GabrielBenchmark.java
 
 #+murmel (load "000_lib.lisp")
+
 #-murmel
 (defmacro define (sym &optional (val nil))
   `(defparameter ,sym ,val))
 
+
 (load "00_tak_apo.lisp")
 (load "3.01_tak.lisp")
 (load "3.01_takeuchi.lisp")
-(load "3.02_stak.lisp")
+#+murmel (load "3.02_stak.lisp")      #-murmel (load "3.02_stak_CL.lisp")
 (load "3.04_takl.lisp")
-#+murmel (load "3.10_deriv.lisp")
-#-murmel (load "3.10_deriv_CL.lisp")
+#+murmel (load "3.10_deriv.lisp")     #-murmel (load "3.10_deriv_CL.lisp")
