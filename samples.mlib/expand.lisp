1230ad4c6871c9cb688d9049a9f131cddb0ae645
Robert Mayer <mayer_robert@gmx.net>
2024-01-28 07:48:46 +0100

    expand.lisp: add a convenience macro

--- samples.mlib/expand.lisp
+++ samples.mlib/expand.lisp
@@ -9,19 +9,24 @@
 ;;;
 ;;;     (pprint (expand 'form))
 ;;;
+;;; or:
+;;;
+;;;     (xp form)
+;;;
 ;;; Only macros already defined before the form will be expanded,
 ;;; new macros defined by defmacro calls within the form are not considered.
 ;;;
 ;;; NOTE: I'm pretty sure that this is incomplete and probably buggy.
 ;;;
-;;; `expand` currently only works with the interpreter (the compiler
+;;; `expand` currently only works with JMurmel's interpreter (the compiler
 ;;; doesn't fully support macroexpand-1).
 
 #+murmel
 (require "mlib")
-(provide "expand")
+
 
 (defun expand (form)
+  "Recursively macroexpand everything."
   (cond ((atom form)
          form)
 
@@ -36,19 +41,19 @@
 
         ((eq 'lambda (car form))
          `(lambda ,(cadr form)
-                  ,@(mapcar #'expand (cddr form))))
+            ,@(mapcar #'expand (cddr form))))
 
         ((member (car form) '(defun defmacro) #-murmel :test #'eq)
          `(,(car form) ,(cadr form)
-                 ,(caddr form)
-                 ,@(mapcar #'expand (cdddr form))))
+           ,(caddr form)
+           ,@(mapcar #'expand (cdddr form))))
 
         ((member (car form) '(let let* letrec) #-murmel :test #'eq)
          (if (symbolp (cadr form))
-               ;; named or dynamic letXX
-               `(,(car form) ,(cadr form) ,(expand (caddr form)) ,@(mapcar #'expand (cdddr form)))
-           ;; normal letXX
-           `(,(car form) ,(expand (cadr form)) ,@(mapcar #'expand (cddr form)))))
+             ;; named or dynamic letXX
+             `(,(car form) ,(cadr form) ,(expand (caddr form)) ,@(mapcar #'expand (cdddr form)))
+             ;; normal letXX
+             `(,(car form) ,(expand (cadr form)) ,@(mapcar #'expand (cddr form)))))
 
         ((eq 'setq (car form))
          `(setq ,(cadr form) ,(expand (caddr form))))
@@ -56,5 +61,13 @@
         (t
          (multiple-value-bind (ex changed) (macroexpand-1 form)
            (if changed
-                 (expand ex)
-             (mapcar #'expand form))))))
+               (expand ex)
+               (mapcar #'expand form))))))
+
+
+(defmacro xp (form)
+  "Convenience macro to pretty-print and recursively macroexpand everything."
+  `(pprint (expand ',form)))
+
+
+(provide "expand")
